eba14fccd8521aa6bb7ac26b7425235868dbaac8
Andre <adriedge@gmail.com>
2019-01-25 00:42:23 -0700

    a2 progress

--- A2-Modelling/genCylinder.py
+++ A2-Modelling/genCylinder.py
@@ -1,10 +1,9 @@
 # Andre Driedger
-
 # Generates a cylinder obj file with radius 1 and height 2
 
 from math import *
 
-n = 8 #discretizations
+n = 6 #discretizations
 vertices = []
 
 def gen_vertices(side):
@@ -22,12 +21,30 @@ gen_vertices("bottom")
 gen_vertices("top")
 #print vertices
 
+faces = []
+
+def gen_faces(side):
+    if side == "bottom":
+        s = 1
+        l = 1
+    elif side =="top":
+        s = 1+n+1
+        l = 2
+    for i in range(s+1, s+n+1):
+        faces.append((s, i, ((i-l)%n)+s+1))
+
+gen_faces("bottom")
+gen_faces("top")
+
 objFile = open("cylinder.obj", "w")
 for v in vertices:
     objFile.write("v ")
     for a in v:
         objFile.write(format(a, ".2f") + " ")#output to a precision of 2
     objFile.write("\n")
-    
-
+for f in faces:
+    objFile.write("f ")
+    for a in f:
+        objFile.write(str(a) + " ")
+    objFile.write("\n")
 
