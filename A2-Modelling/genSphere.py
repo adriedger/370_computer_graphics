b83290a5abf5a131f4a923b1c9e7c2b6c864fb51
Andre <adriedge@gmail.com>
2019-01-27 00:27:03 -0700

    a2 sphere progress

--- A2-Modelling/genSphere.py
+++ A2-Modelling/genSphere.py
@@ -1,5 +1,5 @@
 # Andre Driedger 1805536
-# Generates a shere obj file with radius 1 and 32 discretizations
+# Generates a shere obj file with radius 1
 
 from math import *
 
@@ -12,11 +12,12 @@ def gen_vertices():
     vertices.append((0, 1, 0)) #north pole
     vertices.append((0, -1, 0)) #south pole
     ele_agl = (pi)/m #180 from top to bottom
-    azm_agl = (2*pi)/n #360 counterclockwise from (1, 0, 0)
+    azm_agl = (2*pi)/n #360 clockwise from (1, 0, 0)
     for i in range(0, n):
         theta = azm_agl*i
         for j in range(1, m):
             phi = ele_agl*j
+            #parametric coordinates
             x = cos(theta)*sin(phi)
             y = cos(phi) #cos of the discretized elevation angle
             z = sin(theta)*sin(phi)
@@ -25,23 +26,23 @@ def gen_vertices():
 gen_vertices()
 #print vertices
 faces = []
-'''
-def gen_faces(side): #start/end points of each side are key delimiters
-    if side == "bottom":
-        for i in range(2, 2+n):
-            faces.append((1, i, ((i-1)%n)+2))
-    elif side =="top":
-        for i in range(3+n, 3+n+n):
-            faces.append((2+n, i, ((i-2)%n)+n+3))
-    elif side == "sides":
-        for i in range(2, 2+n):
-            faces.append((i, i+n+1, ((i-1)%n)+n+3))
-            faces.append((i, ((i-1)%n)+n+3, ((i-1)%n)+2))
- 
-gen_faces("bottom")
-gen_faces("top")
-gen_faces("sides")
-'''
+
+def gen_faces():
+
+    #top and bottom part
+    for i in range(0, n-1):
+        a = 1 #north pole
+        b = 3+(15*i)
+        c = b+15
+        faces.append((a, b, c))
+        a = 2 #south pole
+        b = 17+(15*i)
+        c = b+15
+        faces.append((a, b, c))
+
+    
+gen_faces()
+
 
 objFile = open("sphere.obj", "w")
 for v in vertices:
@@ -50,9 +51,9 @@ for v in vertices:
         objFile.write(format(a, ".2f") + " ") #output to a precision of 2
     objFile.write("\n")
 
-'''for f in faces:
+for f in faces:
     objFile.write("f ")
     for a in f:
         objFile.write(str(a) + " ")
     objFile.write("\n")
-'''
+
